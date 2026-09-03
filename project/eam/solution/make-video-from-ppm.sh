99453ec63a62786cc440db5fd4814c13dd01a34a
Simon Gravelle <simon.gravelle@live.fr>
2026-09-03 10:12:11 +0200

    added eam video

---
+++ project/eam/solution/make-video-from-ppm.sh
@@ -0,0 +1,6 @@
+printf '%s\n' myimage-*.ppm | sort -t- -k2,2n | sed "s/^/file '/; s/$/'/" > frames.txt
+
+ffmpeg -y -f concat -safe 0 -i frames.txt     -vf "setpts=N/20/TB"     -c:v libx264 -crf 18 -pix_fmt yuv420p     eam.mp4
+
+rm frames.txt
+
